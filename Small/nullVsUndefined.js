6764832ceedb64baf9b269bb0f9d56914032afc9
NikiKostadinova <niki_radost@yahoo.com>
2024-02-15 23:13:30 +0200

    add

---
+++ Small/nullVsUndefined.js
@@ -0,0 +1,9 @@
+// what will be logged in first and second example?
+
+let variable1;
+console.log(variable1); // undefined
+console.log(typeof variable1); // undefined
+
+let variable2 = null;
+console.log(variable2); //null
+console.log(typeof variable2); // object
